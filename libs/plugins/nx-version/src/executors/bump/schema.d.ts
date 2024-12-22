b507b319dd218da707f67d285a23cf8137661a25
Dimy Jeannot <>
2024-12-21 19:23:40 -0500

    fix: working on github actions

--- libs/plugins/nx-version/src/executors/bump/schema.d.ts
+++
@@ -1,8 +0,0 @@
-export interface BumpExecutorSchema {
-  branchName?: string;
-  commitMessage: string;
-  packageJson: string;
-  baseSha?: string;
-  headSha?: string;
-  checkGitDiff?: boolean;
-}
