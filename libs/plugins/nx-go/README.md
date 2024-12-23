0aa68879a71b3ffaa8653c67c2f1d881d1cea678
Dimy Jeannot <>
2024-12-23 08:10:05 -0500

    fix: working on github actions

---
+++ libs/plugins/nx-go/README.md
@@ -0,0 +1,11 @@
+# nx-go
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-go` to build the library.
+
+## Running unit tests
+
+Run `nx test nx-go` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/plugins/nx-go/jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+  displayName: 'nx-go',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/plugins/nx-go',
+};
