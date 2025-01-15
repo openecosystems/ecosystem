1e3b5dc188ec8879b9b265a3a4ccec1e2ac39e9a
Dimy Jeannot <>
2025-01-15 09:37:56 -0500

    chore: formatting

--- apps/workloads/public/configuration/v2alpha/configuration/main.go
+++ apps/workloads/public/configuration/v2alpha/configuration/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "context"
-  "libs/partner/go/nats/v2"
-  //"libs/partner/go/nebula/v1"
-  "libs/partner/go/zap/v1"
-  "libs/private/go/configuration/v2alpha"
-  "libs/public/go/connector/v2alpha"
-  "libs/public/go/sdk/v2alpha"
+	"context"
+	"libs/partner/go/nats/v2"
+	//"libs/partner/go/nebula/v1"
+	"libs/partner/go/zap/v1"
+	"libs/private/go/configuration/v2alpha"
+	"libs/public/go/connector/v2alpha"
+	"libs/public/go/sdk/v2alpha"
 )
 
 func main() {
-  bounds := []sdkv2alphalib.Binding{
-    &zaploggerv1.Binding{},
-    //&nebulav1.Binding{},
-    &natsnodev2.Binding{SpecEventListeners: []natsnodev2.SpecEventListener{
-      &CreateConfigurationListener{},
-      &GetConfigurationListener{},
-    }},
-    &configurationv2alphalib.Binding{},
-  }
+	bounds := []sdkv2alphalib.Binding{
+		&zaploggerv1.Binding{},
+		//&nebulav1.Binding{},
+		&natsnodev2.Binding{SpecEventListeners: []natsnodev2.SpecEventListener{
+			&CreateConfigurationListener{},
+			&GetConfigurationListener{},
+		}},
+		&configurationv2alphalib.Binding{},
+	}
 
-  connector := connectorv2alphalib.NewConnector(context.Background(), bounds)
-  connector.ListenAndProcess()
+	connector := connectorv2alphalib.NewConnector(context.Background(), bounds)
+	connector.ListenAndProcess()
 }
