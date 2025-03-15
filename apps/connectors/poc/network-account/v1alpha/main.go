f109f2c421cebd6e7f33fa7abe2eb04154d7d31c
Dimy Jeannot <>
2025-03-15 00:12:06 -0400

    refactor: working on publishing package for connectors

---
+++ apps/connectors/poc/network-account/v1alpha/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+
+	internal "github.com/openecosystems/ecosystem/apps/connectors/poc/network-account/v1alpha/internal"
+	listeners "github.com/openecosystems/ecosystem/apps/connectors/poc/network-account/v1alpha/listeners"
+	natsnodev1 "github.com/openecosystems/ecosystem/libs/partner/go/nats"
+	nebulav1 "github.com/openecosystems/ecosystem/libs/partner/go/nebula"
+	zaploggerv1 "github.com/openecosystems/ecosystem/libs/partner/go/zap"
+	configurationv2alphalib "github.com/openecosystems/ecosystem/libs/private/go/configuration/v2alpha"
+	sdkv2alphalib "github.com/openecosystems/ecosystem/libs/public/go/sdk/v2alpha"
+)
+
+func main() {
+	bounds := []sdkv2alphalib.Binding{
+		&zaploggerv1.Binding{},
+		&nebulav1.Binding{},
+		&natsnodev1.Binding{SpecEventListeners: []natsnodev1.SpecEventListener{
+			&listeners.CreateEcosystemListener{},
+		}},
+		&configurationv2alphalib.Binding{},
+	}
+
+	c := &internal.Configuration{}
+
+	connector := sdkv2alphalib.NewConnector(
+		context.Background(),
+		sdkv2alphalib.WithConnectorBounds(bounds),
+		sdkv2alphalib.WithConnectorConfigurationProvider(c),
+	)
+	connector.ListenAndProcess()
+}
